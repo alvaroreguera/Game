ec1d48ba2711327964cc1e6884519d1b5c58a2f2
Alvaro Reguera <alvaro.reguera@gmail.com>
2018-04-05 16:17:28 +0200

    con puerta

--- js/Win.js
+++ js/Win.js
@@ -1,20 +1,34 @@
 function Win(game) {
 
     this.game = game;
-    this.x = this.game.canvas.width/2 + this.game.canvas.width * 0.05;
-    this.y = this.game.canvas.height - 30; //posicion actual 
-    this.width = 20; 
-    this.height = 30; 
+    this.x = this.game.canvas.width / 2 + this.game.canvas.width * 0.05;
+    this.y = this.game.canvas.height - 60; //posicion actual 
+    this.width = 20;
+    this.height = 30;
     this.color = "red";
-// this.img = new Image();
-// this.img.src = "./img/player.png";
-// this.width = 80;
-// this.height = 100;
-// this.animateImg();
+    this.img = new Image();
+    this.img.src = "images/door.png";
+    this.width = 30;
+    this.height = 60;
 }
 
+// Win.prototype.draw = function () {
+//     this.game.ctx.fillStyle = this.color;
+//     this.game.ctx.fillRect(this.x, this.y, this.width, this.height)
+
+// };
+
 Win.prototype.draw = function () {
-    this.game.ctx.fillStyle = this.color;
-    this.game.ctx.fillRect(this.x, this.y, this.width, this.height)
+    this.game.ctx.drawImage(
+        this.img,
+        0,
+        0,
+        this.img.width*0.95,
+        this.img.height*0.95,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+    );
 
 };
